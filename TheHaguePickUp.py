21901439d82b25536b93d7afb1098499eeb48d16
Chris Gerpheide <phoxicle@gmail.com>
2016-06-12 21:59:05 +0200

    GWA-1 Now has session handling to retrieve house number and zip code separately.

--- TheHaguePickUp.py
+++ TheHaguePickUp.py
@@ -17,21 +17,13 @@ def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
+    print("event.session.application.applicationId=" + event['session']['application']['applicationId'])
 
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    if (event['session']['application']['applicationId'] != "amzn1.echo-sdk-ams.app.8b6d1a64-da37-4e3f-ac18-be4f7da53e24"):
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
+        on_session_started({'requestId': event['request']['requestId']}, event['session'])
 
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'])
@@ -42,33 +34,26 @@ def lambda_handler(event, context):
 
 
 def on_session_started(session_started_request, session):
-    """ Called when the session starts """
-
     print("on_session_started requestId=" + session_started_request['requestId']
           + ", sessionId=" + session['sessionId'])
 
 
 def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-
+    """ Called when the user launches the skill without specifying what they want """
     print("on_launch requestId=" + launch_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    # Dispatch to your skill's launch
     return get_welcome_response()
 
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
-
     print("on_intent requestId=" + intent_request['requestId'] +
           ", sessionId=" + session['sessionId'])
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
-    # Dispatch to your skill's intent handlers
+    # Dispatch to intent handlers
     if intent_name == "WhenIntent":
         return get_when_response(intent, session)
     else:
@@ -76,88 +61,99 @@ def on_intent(intent_request, session):
 
 
 def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
-
-    Is not called when the skill returns should_end_session=true
-    """
+    """ Called when the user ends the session. Is not called when the skill returns should_end_session=true """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
     # add cleanup logic here
 
 # --------------- Functions that control the skill's behavior ------------------
 
-
 def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
     card_title = "Welcome"
-    speech_output = "Welcome to The Hague Pick Up" \
-                    "Ask me when your pick up days are by saying, " \
-                    "when in zip code two five six two x l."
+    speech_output = "What is your zip code and house number?"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "Ask me when your pick up days are by saying, " \
-                    "when for two five six two x l."
+            "zip code two five six two x l, house number three."
     should_end_session = False
     return build_response({}, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Bye and keep recycling!"
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-
-
 def get_when_response(intent, session):
-    
+    session_attributes = {}
     card_title = intent['name']
 
+    # Get the zip code, either from user now or from session
+    zip_code = None
     if ('ZipNumber' in intent['slots'] and 'value' in intent['slots'] ['ZipNumber']
         and 'ZipFirstLetter' in intent['slots'] and 'value' in intent['slots'] ['ZipFirstLetter']
-        and 'ZipSecondLetter' in intent['slots'] and 'value' in intent['slots'] ['ZipSecondLetter']
-        and 'HouseNumber' in intent['slots'] and 'value' in intent['slots'] ['HouseNumber']):
+        and 'ZipSecondLetter' in intent['slots'] and 'value' in intent['slots'] ['ZipSecondLetter']):
 
         zip_code = intent['slots']['ZipNumber']['value'] + intent['slots']['ZipFirstLetter']['value'] + intent['slots']['ZipSecondLetter']['value'] 
-        house_number = intent['slots']['HouseNumber']['value'] 
+        print("Got new zip code: " + zip_code)
+        session_attributes['zip_code'] = zip_code
+    elif (session.get('attributes', {}) and "zip_code" in session.get('attributes', {})):
 
-        print("Getting when response for zip {} and house number {}".format(zip_code, house_number))
+        zip_code = session['attributes']['zip_code']
+        print("Using session zip code: " + zip_code)
 
-        # Scrape the pick up days from the website
-        print("Retrieving HTML...")
-        #html = '<div class="assemblySide"><div class="subBody"><div class="itemBlock"><h2>Eerstvolgende inzameldata</h2><ul class="listSmall"><li><img src="/HuisVuilFrontOffice/img/icon_papier.png" alt="Ophaaldag papier"><strong>Papier</strong><br>donderdag 30 juni</li></ul></div><div class="itemBlock"><h2>Legenda</h2><div class="textCnt"><ul class="listSmall"><li><img src="/HuisVuilFrontOffice/img/icon_papier.png" alt="Ophaaldag papier">Ophaaldag Papier</li></ul><br><a id="wt58_wt29" href="http://www.denhaag.nl/afval">Klik hier voor meer informatie over de verschillende soorten afval en het scheiden van uw huisvuil.</a></div></div></div></div>'
-        html = HTMLLoader.get_html(zip_code, house_number)
+    # Get the house number (always gotten from the user, since we don't store it)
+    house_number = None
+    if ('HouseNumber' in intent['slots'] and 'value' in intent['slots'] ['HouseNumber']):
+        house_number = intent['slots']['HouseNumber']['value']
+        print("Got house number: " + house_number)
+
+    if zip_code:
+        
+
        
-        print("Parsing HTML...")
-        parser = PickUpHTMLParser()
-        parser.feed(html)
-
-        data_pairwise = parser.get_data_pairwise()
-
-        print("Translating data...")
-        pick_up_days = []
-        for type_dutch, date_dutch in data_pairwise:
-            try:
-                translated = PickUpTranslator.translate(type_dutch, date_dutch)
-            except:
-                translated = "Translation error"
-            pick_up_days.append(translated)
-
-        # Return the info
-        speech_output = "The next pick up days for {} number {} are: {}".format(zip_code, house_number, ", ".join(pick_up_days))
-        reprompt_text = ""
-        should_end_session = True
+            # No reason to store house_number in session since we always do zip first. Just give them the response.
+            speech_output = calculate_when_response(zip_code, house_number)
+            reprompt_text = ""
+            should_end_session = True
+        else:
+            print("Got zip code, but no house number. Prompting.")
+            speech_output = "Which house number?"
+            reprompt_text = "Ask me when your pick up days are by saying, " \
+                        "house number three."
+            should_end_session = False
     else:
-        speech_output = "I didn't hear a Dutch zip code. Please try again."
+        speech_output = "Which zip code and house number?"
         reprompt_text = "Ask me when your pick up days are by saying, " \
-                    "when for two five six two x l."
+                    "zip code two five six two x l, house number three."
         should_end_session = False
-    return build_response({}, build_speechlet_response(
+    return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+
+def calculate_when_response(zip_code, house_number):
+    print("Getting when response for zip {} and house number {}".format(zip_code, house_number))
+    text = ""
+
+    # Scrape the pick up days from the website
+    print("Retrieving HTML...")
+    html = HTMLLoader.get_html(zip_code, house_number)
+   
+    print("Parsing HTML...")
+    parser = PickUpHTMLParser()
+    try:
+        parser.feed(html)
+        data_pairwise = parser.get_data_pairwise()
+    except:
+        print("Error parsing HTML")
+        text = "Error fetching HTTP response"
+
+    print("Translating data...")
+    pick_up_days = []
+    for type_dutch, date_dutch in data_pairwise:
+        try:
+            translated = PickUpTranslator.translate(type_dutch, date_dutch)
+        except:
+            translated = "Translation error"
+        pick_up_days.append(translated)
+
+    text = "The next pick up days for {} number {} are: {}".format(zip_code, house_number, ", ".join(pick_up_days))
+    print("Calculated full text: " + text)
+    return text
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -189,10 +185,6 @@ def build_response(session_attributes, speechlet_response):
         'sessionAttributes': session_attributes,
         'response': speechlet_response
     }
-    
-def log(str):
-    #print str
-    print(str)
 
 class PickUpHTMLParser(HTMLParser):
 
@@ -206,10 +198,10 @@ class PickUpHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if PickUpHTMLParser.in_inzameldata:
-            log("Encountered some data  :" + data)
+            print("Encountered some data  :" + data)
             PickUpHTMLParser.data.append(data)
         if data == "Eerstvolgende inzameldata":
-            log("in inzameldata")
+            print("in inzameldata")
             PickUpHTMLParser.in_inzameldata = True
 
     def get_data_pairwise(self):
@@ -247,7 +239,7 @@ class PickUpTranslator:
 
     @staticmethod
     def translate(type_dutch, date_dutch):
-        log("Traslating type '{}' and date '{}'".format(type_dutch, date_dutch))
+        print("Translating type '{}' and date '{}'".format(type_dutch, date_dutch))
         # Date Ex: donderdag 30 juni
         [dayofweek_dutch, numday, month_dutch] = date_dutch.split()
         translated = '{} on {}, {} {};'.format(
@@ -255,7 +247,7 @@ class PickUpTranslator:
             PickUpTranslator.days[dayofweek_dutch.lower()], 
             PickUpTranslator.months[month_dutch.lower()] ,
             numday)
-        log("Translated to: " + translated)
+        print("Translated to: " + translated)
         return translated
 
 # Retrieve URLs like: http://huisvuilkalender.denhaag.nl/HuisVuilFrontOffice/Resultaat/2562XL/108/+/11-06-2016/11-07-2016
